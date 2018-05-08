b28f86f80b8db05afd657f3a1d5c7d56017c2504
glietzen@gmail.com <glietzen@gmail.com>
2018-05-07 19:04:39 -0500

    Update init files
    1. Setup Seqeulize ORM
    2. Updated connection.js for local development
    3. Corrected schema.sql floats
    4. Configured index.js for Sequelize ORM
    5. Setup schema for table in Sequelize ORM
    6. Installed NPM dependencies
    7. Setup POST new Project route for development
    8. Configured server.js to install dependencies, parse application, set routes, sync Sequelize, and start app

---
+++ server.js
@@ -0,0 +1,26 @@
+// REQUIRE DEPENDENCIES
+// ============================================
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./models');
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+// PARSE APPLICATION
+// ============================================
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES
+// ============================================
+require('./routes/api-routes')(app);
+
+// SYNC SEQUELIZE & START APP
+//============================================
+db.sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on PORT ${PORT}`);
+    });
+});
+
