0d206a8f3c22eccf5699b9716a44c62f9760631d
glietzen@gmail.com <glietzen@gmail.com>
2018-05-08 08:20:38 -0500

    Setup HTML routes

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = (app) => {
+
+    // GET ROUTES
+    // =======================================
+    
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      });
+
+
+
+
+
+
+}
