bf16c9a69bbbb3324e7c476edff5c5659db9a23d
Stetson Ramey <stetson.ramey@gmail.com>
2018-05-09 18:29:51 -0500

    1. added projects route in html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,6 +8,11 @@ module.exports = (app) => {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
       });
+    
+    // STETSON'S PROJECT PAGE ROUTE =====================================
+    app.get("/projects", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+    });
 
 
 
