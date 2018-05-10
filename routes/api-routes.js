36bd74475f099faba456767c400cab143639e563
glietzen@gmail.com <glietzen@gmail.com>
2018-05-09 21:06:28 -0500

    updated dashboard and ajax

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,8 +2,19 @@ const db = require('./../models');
 
 module.exports = (app) => {
 
-    // POST ROUTES
-    // ============================================
+// GET ROUTES
+// ============================================
+
+    // GET ALL DATA FROM DATABASE
+    app.get('/api/project/all', (req,res) => {
+        db.costs.findAll({}).then((results) => {
+            res.json(results);
+        });
+    });
+
+
+// POST ROUTES
+// ============================================
 
     // POST NEW ROW
     app.post('/api/project/new', (req, res) => {
