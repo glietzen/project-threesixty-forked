ddfefea8f5886f178ef4734a2811f879fdcfa85c
Stetson Ramey <stetson.ramey@gmail.com>
2018-05-12 11:59:47 -0500

    1. wrote js to get jobs from database 2. added GET route for all projects

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,3 +1,80 @@
 $(document).ready(function () {
   
-})
+  // jobContainer holds all of our posts
+  var jobContainer = $(".job-container");
+  var jobStatusSelect = $("#status");
+
+// This function grabs posts from the database and updates the view
+function getJobs(status) {
+  var statusString = status || "";
+  if (statusString) {
+    statusString = "/status/" + statusString;
+  }
+  $.get("/api/projects" + statusString, function(data) {
+    console.log("Projects", data);
+    projects = data;
+    if (!projects || !projects.length) {
+      displayEmpty();
+    }
+    else {
+      initializeRows();
+    }
+  });
+}
+
+// GET ALL THE JOBS
+getJobs();
+
+  // InitializeRows handles appending all of our constructed PROJECT HTML inside
+  // jobContainer
+  function initializeRows() {
+    jobContainer.empty();
+    var projectsToAdd = [];
+    for (var i = 0; i < projects.length; i++) {
+      projectsToAdd.push(createNewRow(projects[i]));
+    }
+    jobContainer.append(projectsToAdd);
+  }
+
+// This function displays a message when there are no jobs
+function displayEmpty() {
+  jobContainer.empty();
+  var messageH2 = $("<h2>");
+  messageH2.css({ "text-align": "center", "margin-top": "50px" });
+  messageH2.html("No projects yet, navigate <a href='/new-project'>here</a> in order to create a new job.");
+  jobContainer.append(messageH2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});// end document.ready function ------------------>>
+
+
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,13 +5,21 @@ module.exports = (app) => {
     // POST ROUTES
     // ============================================
 
+
+    // GET ROUTE FOR GRABBING ALL THE PROJECTS
+    app.get("/api/projects/", (req, res) => {
+        db.costs.findAll({})
+        .then((dbPost) => {
+            res.json(dbPost);
+        });
+    });
+
     // POST NEW ROW
     app.post('/api/project/new', (req, res) => {
         db.costs.create(req.body).then((dbPost) => {
             res.json(dbPost);
         });
     });
-    
     
     // PUT ROUTE FOR UPDATING A ROW
     app.put('/api/project/new', (req, res) => {
