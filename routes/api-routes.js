a09a2dbd81548a0e7580cfa739b6ceee27da1a34
Stetson <stetson.ramey@gmail.com>
2018-05-12 12:03:32 -0500

    Stets project page (#8)
    updates to project page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,10 +16,32 @@ module.exports = (app) => {
 // POST ROUTES
 // ============================================
 
+
+    // GET ROUTE FOR GRABBING ALL THE PROJECTS
+    app.get("/api/projects/", (req, res) => {
+        db.costs.findAll({})
+        .then((dbPost) => {
+            res.json(dbPost);
+        });
+    });
+
     // POST NEW ROW
     app.post('/api/project/new', (req, res) => {
         db.costs.create(req.body).then((dbPost) => {
             res.json(dbPost);
+        });
+    });
+    
+    // PUT ROUTE FOR UPDATING A ROW
+    app.put('/api/project/new', (req, res) => {
+        db.costs.update(req.body,
+        {
+            where: {
+                id: req.body.id
+            }
+        })
+        .then((dbPost) => {
+            res.json(dbPost)
         });
     });
 
@@ -28,4 +50,4 @@ module.exports = (app) => {
 
 
 
-}
+};
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,6 +8,14 @@ module.exports = (app) => {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
       });
+    
+    // STETSON'S PROJECT PAGE ROUTES =====================================
+    app.get("/projects", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+    });
+    app.get("/new-project", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/new-project.html"));
+    });
 
     app.get("/dashboard", function(req,res) {
         res.sendFile(path.join(__dirname, "../public/dashboard.html"))
